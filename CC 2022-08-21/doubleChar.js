64b09b884235925356b6f43ae56b22b3841946eb
Shamari Lewis <superskl5000@gmail.com>
2023-08-22 14:47:45 -0700

    Updated parameters to doing challenge from 08-21.

--- CC 2022-08-21/doubleChar.js
+++ CC 2022-08-21/doubleChar.js
@@ -1,4 +1,4 @@
-// Code Wars Coding Challenge
+// Coding Challenge
 // Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
 // Examples (Input -> Output):
